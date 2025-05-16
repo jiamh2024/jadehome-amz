1514012c9e8681efde4a8ef89042ad2bedb3a875
jiamh2024 <jia@homina.top>
2025-05-16 15:56:56 +0800

    modified:   db/db.js

--- db/db.js
+++ db/db.js
@@ -2,8 +2,9 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: "192.168.30.6",
-  user: "jia",
+//  host: "192.168.30.6",
+host: "localhost",
+user: "jia",
   password: "123",
   database: "jdb",
   port: 3306,
